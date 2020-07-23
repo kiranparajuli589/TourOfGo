b349d60c7780b175a513e9f1960cf87cfd5e60f5
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-07-23 11:28:20 +0545

    Added struct feature description

--- playground.go
+++ playground.go
@@ -1,16 +1,14 @@
 package main
-
 import "fmt"
-
-func main()  {
-	i,j := 42, 2701
-	p := &i     // point to i
-	fmt.Println(p) // address of i
-	fmt.Println(*p)     // read i through the pointer
-	*p = 21     // set i through the pointer
-	fmt.Println(i)      // see the new value of i
-
-	p = &j      // point to j
-	*p = *p / 37    // divide j through the pointer
-	fmt.Println(j) // see the new value of j
+type Vertex struct{
+	X, Y int
+}
+var (
+	v1 = Vertex{1, 2}     // has type Vertex
+	v2 = Vertex{X:1}      // Y:0 implicit
+	v3 = Vertex{}         // X:0 and Y:0
+	p = &Vertex{1,2}      // has type *Vertex
+)
+func main() {
+	fmt.Println(v1, p, v2, v3)
 }
